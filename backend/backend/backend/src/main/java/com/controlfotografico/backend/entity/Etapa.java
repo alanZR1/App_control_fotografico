0b26d98038d6adebf7d23676d22641169c506739
Alan Reyes Luis <alannreyes02@gmail.com>
2026-08-04 13:50:23 -0600

    entidad etapas creada

--- backend/backend/backend/src/main/java/com/controlfotografico/backend/entity/Etapa.java
+++ backend/backend/backend/src/main/java/com/controlfotografico/backend/entity/Etapa.java
@@ -1,5 +1,33 @@
 package com.controlfotografico.backend.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "etapas")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Etapa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_etapa")
+    private Long idEtapa;
+
+    @Column(name = "nombre", nullable = false, length = 100)
+    private String nombre;
+
+    @Column(name = "orden", nullable = false)
+    private Integer orden;
 
 }
