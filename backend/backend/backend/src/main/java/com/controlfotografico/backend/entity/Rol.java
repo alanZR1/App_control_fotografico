5407739ad0c69e32744d64261530d714789024ee
Alan Reyes Luis <alannreyes02@gmail.com>
2026-08-04 13:31:13 -0600

    configuracion inicial creacion de entidad Rol

--- backend/backend/backend/src/main/java/com/controlfotografico/backend/entity/Rol.java
+++ backend/backend/backend/src/main/java/com/controlfotografico/backend/entity/Rol.java
@@ -1,5 +1,43 @@
 package com.controlfotografico.backend.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Rol {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(
+        name = "id_rol"
+    )
+    private Long idRol;
+
+    @Column(
+        name = "nombre", 
+        nullable = false, 
+        unique = true, 
+        length = 50
+    )
+    private String nombre;
+
+    @Column(
+        name = "descripcion"
+    )
+    private String descripcion;
 
 }
