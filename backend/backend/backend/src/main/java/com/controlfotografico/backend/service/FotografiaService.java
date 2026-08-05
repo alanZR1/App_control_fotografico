469fcc69e36a5dee601fc191ad15dcd409300b06
Alan Reyes Luis <alannreyes02@gmail.com>
2026-08-05 14:42:28 -0600

    implementacion de FotografiaService

--- backend/backend/backend/src/main/java/com/controlfotografico/backend/service/FotografiaService.java
+++ backend/backend/backend/src/main/java/com/controlfotografico/backend/service/FotografiaService.java
@@ -1,5 +1,20 @@
 package com.controlfotografico.backend.service;
 
+import java.util.List;
+import java.util.UUID;
+
+import com.controlfotografico.backend.entity.Fotografia;
+
 public interface FotografiaService {
+
+    List<Fotografia> listar();
+
+    Fotografia buscarPorId(UUID id);
+
+    Fotografia guardar(Fotografia fotografia);
+
+    Fotografia actualizar(UUID id, Fotografia fotografia);
+
+    void eliminar(UUID id);
 
 }
