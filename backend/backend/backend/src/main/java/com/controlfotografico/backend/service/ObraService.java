1cb185d77d51c7b6850d97fc35899d3556f6b331
Alan Reyes Luis <alannreyes02@gmail.com>
2026-08-05 14:41:56 -0600

    implementacion de ObraService

--- backend/backend/backend/src/main/java/com/controlfotografico/backend/service/ObraService.java
+++ backend/backend/backend/src/main/java/com/controlfotografico/backend/service/ObraService.java
@@ -1,5 +1,19 @@
 package com.controlfotografico.backend.service;
 
+import java.util.List;
+
+import com.controlfotografico.backend.entity.Obra;
+
 public interface ObraService {
+
+    List<Obra> listar();
+
+    Obra buscarPorId(Long id);
+
+    Obra guardar(Obra obra);
+
+    Obra actualizar(Long id, Obra obra);
+
+    void eliminar(Long id);
 
 }
