fdf6010452361237b72ef250f66cc1b6fd0bb61e
Alan Reyes Luis <alannreyes02@gmail.com>
2026-08-05 14:43:00 -0600

    implementacion de CatalogoService

--- backend/backend/backend/src/main/java/com/controlfotografico/backend/service/CatalogoService.java
+++ backend/backend/backend/src/main/java/com/controlfotografico/backend/service/CatalogoService.java
@@ -1,5 +1,17 @@
 package com.controlfotografico.backend.service;
 
+import java.util.List;
+
+import com.controlfotografico.backend.entity.Etapa;
+import com.controlfotografico.backend.entity.Rol;
+import com.controlfotografico.backend.entity.TipoFotografia;
+
 public interface CatalogoService {
+
+    List<Rol> listarRoles();
+
+    List<Etapa> listarEtapas();
+
+    List<TipoFotografia> listarTiposFotografia();
 
 }
