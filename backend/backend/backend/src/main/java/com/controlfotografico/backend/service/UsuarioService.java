5f7ad49b83d1852f68c9956c9724a9d1f7ba20b6
Alan Reyes Luis <alannreyes02@gmail.com>
2026-08-05 14:41:10 -0600

    implementacion de UsuarioService

--- backend/backend/backend/src/main/java/com/controlfotografico/backend/service/UsuarioService.java
+++ backend/backend/backend/src/main/java/com/controlfotografico/backend/service/UsuarioService.java
@@ -1,5 +1,19 @@
 package com.controlfotografico.backend.service;
 
+import java.util.List;
+
+import com.controlfotografico.backend.entity.Usuario;
+
 public interface UsuarioService {
+
+    List<Usuario> listar();
+
+    Usuario buscarPorId(Long id);
+
+    Usuario guardar(Usuario usuario);
+
+    Usuario actualizar(Long id, Usuario usuario);
+
+    void eliminar(Long id);
 
 }
