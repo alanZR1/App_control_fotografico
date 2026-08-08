7c95c59eb4490c48a820358d848660c28a60f03b
Alan Reyes Luis <alannreyes02@gmail.com>
2026-08-08 15:35:07 -0600

    endpoint de autenticacion basica (pendiente final)

--- backend/backend/backend/src/main/java/com/controlfotografico/backend/controller/AuthController.java
+++ backend/backend/backend/src/main/java/com/controlfotografico/backend/controller/AuthController.java
@@ -1,5 +1,28 @@
 package com.controlfotografico.backend.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.controlfotografico.backend.service.AuthService;
+
+@RestController
+@RequestMapping("/api/auth")
+@CrossOrigin(origins = "*")
 public class AuthController {
 
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(
+            @RequestParam String correo,
+            @RequestParam String password) {
+
+        return ResponseEntity.ok(
+                authService.login(correo, password)
+        );
+    }
 }
